d495ab1e81cd012a71947cafbc07ff8092b6f457
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-09 16:55:51 +0200

    Code refactore. Fix mobile, fix layout margins, fix links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <footer className="footer">
-            <div className="container">
-                <p className="footer__cotyright">© 2020 Hyperion Mission, All Rights Reserved.</p>
+            <div className="footer__container container">
+                <p className="footer__cotyright">&copy;2020 Hyperion Mission, All Rights Reserved.</p>
                 <ul className="footer__nav">
                     <li className="footer__nav-item">
-                        <a href="#about" className="footer__nav-link">About</a>
+                        <Link to="/main" className="footer__nav-link">Home</Link>
                     </li>
                     <li className="footer__nav-item">
-                        <a href="#workflow" className="footer__nav-link">Workflow</a>
+                        <Link to="/about" className="footer__nav-link">About</Link>
                     </li>
                     <li className="footer__nav-item">
-                        <a href="#source" className="footer__nav-link">Source</a>
+                        <a href="https://www.figma.com/file/GYckFgBjbuJWbPUBDg0sUA/Hyperion?node-id=0%3A1" target="_blank" className="footer__nav-link">Workflow</a>
+                    </li>
+                    <li className="footer__nav-item">
+                        <a href="https://github.com/marianasvirzhevska/hyperion/commits/develop" target="_blank" className="footer__nav-link">Source</a>
                     </li>
                 </ul>
             </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
-import React, {Fragment} from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import story_1 from '../assets/images/story-1.jpg';
 import story_2 from '../assets/images/story-2.jpg';
@@ -7,60 +8,64 @@ import story_4 from '../assets/images/story-4.jpg';
 
 function Main() {
     return (
-        <Fragment>
+        <>
             <section className="hero">
                 <div className="hero__body">
                     <h1 className="hero__title">Hyperion</h1>
                     <p className="hero__description">In the 29th century,
                     the Hegemony of Man comprises thousands of planets
                         connected by farcaster portals.</p>
-                    <a href="#start" className="hero__btn">Start journey</a>
+                    <Link to="/about" className="hero__btn">Start journey</Link>
                 </div>
             </section>
             <div className="container">
                 <section className="story">
-                    <div className="story__wrapper">
+                    <div className="story__wrapper align-left">
                         <div className="story__images">
-                            <img src={story_1} className="story__image" alt="space suit" />
-                            <img src={story_2} className="story__image" alt="space suit helmet" />
+                            <div className="story__images-wrapper">
+                                <img src={story_1} className="story__image" alt="space suit" />
+                                <img src={story_2} className="story__small-image" alt="space suit helmet" />
+                            </div>
                         </div>
                         <div className="story__text">
                             <h3 className="story__title">Some title</h3>
                             <p className="story__description">The Hegemony maintains an uneasy
                             alliance with the TechnoCore, a civilisation of AIs. Modified
                             humans known as Ousters live in space stations between stars and
-                            are engaged in conflictt with the Hegemony.</p>
+                            are engaged in conflict with the Hegemony.</p>
                             <p className="story__description">Numerous "Outback" planets have
-                            no farcasters and cannot be accessed without incurring significant
+                            no far casters and cannot be accessed without incurring significant
                             time dilation. One of these planets is Hyperion, home to
                             structures known as the Time Tombs, which are moving backwards
                             in time and guarded by a legendary creature known as the Shrike.</p>
                         </div>
                     </div>
-                    <div className="story__wrapper">
+                    <div className="story__wrapper align-right">
                         <div className="story__text">
                             <h3 className="story__title">Some title</h3>
                             <p className="story__description">The Hegemony maintains an uneasy
                             alliance with the TechnoCore, a civilisation of AIs. Modified
                             humans known as Ousters live in space stations between stars and
-                            are engaged in conflictt with the Hegemony.</p>
+                            are engaged in conflict with the Hegemony.</p>
                         </div>
                         <div className="story__images">
-                            <img src={story_3} className="story__image" alt="three humans" />
-                            <img src={story_4} className="story__image" alt="two humans" />
+                            <div className="story__images-wrapper">
+                                <img src={story_3} className="story__image" alt="three humans" />
+                                <img src={story_4} className="story__small-image" alt="two humans" />
+                            </div>
                         </div>
                     </div>
                 </section>
             </div>
             <section className="story-start">
-                <div className="story-start__body">
+                <div className="story-start__container container">
                     <h3 className="story-start__title">Each person may cause changes</h3>
                     <p className="story-start__description">Are you ready to take part in a great mission?</p>
-                    <a href="#start" className="story-start__btn">Start journey</a>
+                    <Link to="/about" className="story-start__btn">Start journey</Link>
                 </div>
             </section>
             <Footer/>
-        </Fragment>
+        </>
     )
 }
 
