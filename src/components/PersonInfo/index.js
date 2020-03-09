7d6e83f94470790ea606e951f098debe373f768f
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-09 22:37:04 +0200

    Add simple hints to PersonInfo page

--- src/components/PersonInfo/index.js
+++ src/components/PersonInfo/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router';
 import { team } from '../../team';
+// import RomanInfo from './RomanInfo';
 
 function PersonInfo() {
     const { params } = useRouteMatch();
     const id = params.id;
     const member = team.find(member => member.id === id);
 
+    if (member.page) {
+        return (
+            <>
+                Put your personalize page here
+                {/*<RomanInfo />*/}
+            </>
+        )
+    }
+
     return(
         <div>
-            PersonInfo
-            {member.name}
+            <h2>PersonInfo</h2>
+            <p>{member.name}</p>
+            {/* According to the Figma layout */}
+
+
+            {/* How to add your own component to existing layout? */}
+            {member.name === 'Aleksandr' ? (
+                <p>Your additional component will be here</p>
+            ): null}
         </div>
     )
 }
--- src/team.js
+++ src/team.js
@@ -5,9 +5,9 @@ import mariana from './assets/images/member-4.png';
 import alex from './assets/images/member-5.png';
 
 export const team = [
-    {id: '2', name: 'Nykyta', surname: 'Hlukhov', character: 'Naruto Uzumaki', image: nykyta, info: 'IMSF’s geochemist and hydrologist who supervises all water operations at Olympus Town'},
-    {id: '1', name: 'Katerina', surname: 'Boiko', character: 'Ashley Greene', image: kate, info: 'Ashley Greene is 20 and she lives on the planet Zhytomberd.'},
-    {id: '3', name: 'Mariana', surname: 'Svirzhevska', character: 'Brawne Lamia', image: mariana, info: 'A private investigator from the industrial, high-gravity planet of Lusus.'},
-    {id: '4', name: 'Roman', surname: 'Tsakhlo', character: '', image: roman, info: 'Lenar Hoyt was a Jesuit priest, famous for his role in the final Shrike pilgrimage'},
-    {id: '5', name: 'Aleksandr', surname: 'Bezdorozhev', character: 'Alex Callisto', image: alex, info: 'A Jewish scholar and professor from Barnard\'s World'}
+    {id: '2', name: 'Nykyta', surname: 'Hlukhov', page: false, character: 'Naruto Uzumaki', image: nykyta, info: 'IMSF’s geochemist and hydrologist who supervises all water operations at Olympus Town'},
+    {id: '1', name: 'Katerina', surname: 'Boiko', page: false, character: 'Ashley Greene', image: kate, info: 'Ashley Greene is 20 and she lives on the planet Zhytomberd.'},
+    {id: '3', name: 'Mariana', surname: 'Svirzhevska', page: false, character: 'Brawne Lamia', image: mariana, info: 'A private investigator from the industrial, high-gravity planet of Lusus.'},
+    {id: '4', name: 'Roman', surname: 'Tsakhlo', page: true,  character: '', image: roman, info: 'Lenar Hoyt was a Jesuit priest, famous for his role in the final Shrike pilgrimage'},
+    {id: '5', name: 'Aleksandr', surname: 'Bezdorozhev', page: false, character: 'Alex Callisto', image: alex, info: 'A Jewish scholar and professor from Barnard\'s World'}
 ];
