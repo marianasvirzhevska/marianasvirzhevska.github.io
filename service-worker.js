2fdaf897db09214693b2046dc4df8b1d1d9a22c1
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-11 22:37:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marianasvirzhevska.github.io/precache-manifest.8b4930d7f9d8a4df79c30d11dab693c8.js"
+  "/hyperion/precache-manifest.18a5c4455c7592b065e94c1e2ae6665c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marianasvirzhevska.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hyperion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
