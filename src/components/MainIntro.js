9ab7427da7f9e7ce846ac84fdee2a68a1bb948aa
Nikita Hlukhov <zholud92@gmail.com>
2020-03-12 11:39:18 +0200

    mainintro

--- src/components/MainIntro.js
+++ src/components/MainIntro.js
@@ -3,29 +3,33 @@ import { Link } from "react-router-dom";
 import StarfieldAnimation from 'react-starfield-animation';
 
 function MainIntro () {
-
+  
     return (
       <div>
-        <section className="hero">
-          <StarfieldAnimation
-            numParticles={500}
-            className="hero__starfieldAnimation"
-          />
-          <div>
-            <div className="hero__body">
-              <h1 className="hero__title">Hyperion</h1>
-              <p className="hero__description">
-                In the 34th century, humanity has thousands of planets, it is
-                called Hegemony. Hegemony is extremely advanced and powerful,
-                but there are some problems ...
-              </p>
-              <Link to="/about" className="hero__btn">
-                Start journey
-              </Link>
+          <div className="backgroundWrapper">
+          <section className="hero">
+            <StarfieldAnimation
+              numParticles={500}
+              className="hero__starfieldAnimation"
+            />
+            <div>
+              <div className="hero__body">
+                <h1 className="hero__title">Hyperion</h1>
+                <p className="hero__description">
+                  In the 34th century, humanity has thousands of planets, it is
+                  called Hegemony. Hegemony is extremely advanced and powerful,
+                  but there are some problems ...
+                </p>
+                <Link to="/about" className="hero__btn">
+                  Start journey
+                </Link>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
+
+      
     );
 }
 
