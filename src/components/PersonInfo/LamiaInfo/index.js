c953f253a58447ebbeaf4ebbe5e22dffcf289e52
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-12 16:06:02 +0200

    Styles for personal page

--- src/components/PersonInfo/LamiaInfo/index.js
+++ src/components/PersonInfo/LamiaInfo/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import styles from './index.module.scss';
 
-
 const LamiaInfo = () => {
+    const skillset = [
+        {label: 'Intellect', range: 78},
+        {label: 'Negotiate', range: 17},
+        {label: 'Empathy', range: 88},
+        {label: 'Emotions', range: 45},
+        {label: 'Flexibility', range: 78},
+    ];
+
+    const getRange = (range) => {
+        const style = {};
+        if (range < 20){
+            style.backgroundColor = 'rgba(244, 56, 54, 0.5)';
+        }
+        if (range > 80){
+            style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
+        }
+
+        style.height = (range*200)/100;
+
+        return style
+    };
+
+
     return (
         <div className={styles.root}>
             <div className="container">
@@ -18,8 +40,17 @@ const LamiaInfo = () => {
                     Brawne is a kind of private person who doesn’t want to talk much and share her feeling, you never know what thoughts is on her mind, mystery girl, to admit she is strong willed and confident, also she is great leader but at least now she doesn’t know it yet.
                 </div>
                 <h3 className={styles.subtitle}>Skills set</h3>
-                <div>
-
+                <div className={styles.dashboard}>
+                    {
+                        skillset.map((item, i) => {
+                            return (
+                                <div className={styles.skillItem} key={i}>
+                                    <div className={styles.range} style={getRange(item.range)}>{item.range} %</div>
+                                    <div className={styles.label}>{item.label}</div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
