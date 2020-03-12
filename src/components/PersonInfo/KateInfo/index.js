a92b50eadb7a261434c94857c6abf46ca0a2135a
Kateryna Boiko <Kateryna_Boiko1@epam.com>
2020-03-12 12:05:03 +0200

    profile

--- src/components/PersonInfo/KateInfo/index.js
+++ src/components/PersonInfo/KateInfo/index.js
@@ -1,74 +1,16 @@
 import React from "react";
 import styles from "./index.module.scss";
+import {squaresStyle, dataForChart} from './KateData';
 
-let boxesStyle = [
-    {
-        left: "86%",
-        width: "80px",
-        height: "80px",
-        animationDuration: "0s"
-    },
-    {
-        left: "2%",
-        width: "30px",
-        height: "30px",
-        animationDelay: "1.5s",
-        animationDuration: "10s"
-    },
-    {
-        left: "70%",
-        width: "100px",
-        height: "100px",
-        animationDelay: "7.5s"
-    },
-    {
-        left: "40%",
-        width: "150px",
-        height: "150px",
-        animationDelay: "7.5s",
-        animationDuration: "25s"
-    },
-    {
-        left: "65%",
-        width: "40px",
-        height: "40px"
-    },
-    {
-        left: "8%",
-        width: "110px",
-        height: "110px",
-        animationDelay: "3.5s"
-    },
-    {
-        left: "95%",
-        width: "40px",
-        height: "40px",
-        animationDelay: "1.5s",
-        animationDuration: "10s"
-    }
-];
-
-let dataForStyle = [
-    {
-        color: "#d44",
-        degrees: "190deg",
-        text: "Books"
-    },
-    {
-        color: "#fc3",
-        degrees: "300deg",
-        text: "Music"
-    },
-    {
-        color: "#ac0",
-        degrees: "0",
-        text: "Carelessness"
-    }
-];
 function getStyleForPie() {
+    const a = "conic-gradient(" + dataForChart.map(el => {
+        const {color, degrees} = el;
+        return `${color} 0 ${degrees},`;
+    });
+    console.log(a);
     let result = "conic-gradient(";
-    for (let i = 0; i < dataForStyle.length; i++) {
-        result += `${dataForStyle[i].color} 0 ${dataForStyle[i].degrees},`;
+    for (let i = 0; i < dataForChart.length; i++) {
+        result += `${dataForChart[i].color} 0 ${dataForChart[i].degrees},`;
     }
     result = result.replace(/.$/, ")");
     return { backgroundImage: result };
@@ -111,7 +53,7 @@ function KateInfo() {
                 <div className={styles.pie_chart}>
                     <div className={styles.pie} style={getStyleForPie()}></div>
                     <div className={styles.pie_denotation}>
-                        {dataForStyle.map((el, index) => (
+                        {dataForChart.map((el, index) => (
                             <div key={index}>
                                 <div className={styles.denotation_color} style={{backgroundColor: el.color}}></div>
                                 <span className={styles.paragraph}>{el.text}</span>
@@ -135,7 +77,7 @@ function KateInfo() {
                 </ul>
                 <p className={styles.paragraph}></p>
                 <ul className={styles.box_area}>
-                    {boxesStyle.map((el, index) => (
+                    {squaresStyle.map((el, index) => (
                         <li style={el} key={index}></li>
                     ))}
                 </ul>
