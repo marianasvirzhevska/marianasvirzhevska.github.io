4878337e7e9a0b54c35b5deb0a76c7aad858f28e
Kateryna Boiko <Kateryna_Boiko1@epam.com>
2020-03-12 13:22:42 +0200

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
     BrowserRouter as Router,
     Route,
     Redirect,
     Switch
 } from "react-router-dom";
-import Main from "./components/Main";
-import About from "./components/About";
-import PersonInfo from "./components/PersonInfo";
-import Footer from "./components/Footer";
-import ScrollToTop from "react-router-scroll-top";
+import ScrollToTop from "./ScrollToTop";
+
+const Main = lazy(() => import("./components/Main"));
+const About = lazy(() => import("./components/About"));
+const PersonInfo = lazy(() => import("./components/PersonInfo"));
+const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <ScrollToTop>
-                    <Switch>
-                        <Route path="/main" component={Main} />
-                        <Route exact path="/about" component={About} />
-                        <Route
-                            exact
-                            path="/person-info/:id"
-                            component={PersonInfo}
-                        />
-                        <Redirect exact path="/" to="/main" />
-                    </Switch>
-                    <Footer />
+                    <Suspense fallback={null}>
+                        <Switch>
+                            <Route path="/main">
+                                <Main />
+                            </Route>
+                            <Route exact path="/about">
+                                <About />
+                            </Route>
+                            <Route exact path="/person-info/:id">
+                                <PersonInfo />
+                            </Route>
+                            <Redirect exact path="/" to="/main" />
+                        </Switch>
+                        <Footer />
+                    </Suspense>
                 </ScrollToTop>
             </Router>
         </div>
